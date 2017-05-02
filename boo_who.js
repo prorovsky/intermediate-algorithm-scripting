3c5bf0de22c525475cd591633ac4098b0ec1003b
Pavel Prorovsky <prorovsky@gmail.com>
2017-05-02 18:13:33 +0300

    Add 'Boo Who' challenge

---
+++ boo_who.js
@@ -0,0 +1,36 @@
+/*
+
+Check if a value is classified as a boolean primitive. Return true or false.
+
+Boolean primitives are true and false.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+    Boolean Objects https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Boolean
+
+*/
+
+function booWho(bool) {
+    // What is the new fad diet for ghost developers? The Boolean.
+
+    if(typeof(bool) === "boolean"){
+        console.log(true);
+    }
+    console.log(false);
+    // return bool;
+}
+
+booWho(null);
+
+booWho(true);       // should return true.
+booWho(false);      // should return true.
+booWho([1, 2, 3]);  // should return false.
+booWho([].slice);   // should return false.
+booWho({ "a": 1 }); // should return false.
+booWho(1);          // should return false.
+booWho(NaN);        // should return false.
+booWho("a");        // should return false.
+booWho("true");     // should return false.
+booWho("false");    // should return false. 
