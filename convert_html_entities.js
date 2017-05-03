6c68946151f33bfa7347575da14f7e1fd8e3ff15
Pavel Prorovsky <prorovsky@gmail.com>
2017-05-03 15:15:51 +0300

    Add 'Convert HTML Entities' challenge

---
+++ convert_html_entities.js
@@ -0,0 +1,54 @@
+/*
+
+Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+    RegExp https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp
+
+    HTML Entities https://dev.w3.org/html5/html-author/charref
+
+    String.prototype.replace() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace
+
+*/
+
+function convertHTML(str) {
+    // &colon;&rpar;
+
+    let strArr = str.split("");
+
+    for(let i = 0; i < strArr.length; i++) {
+        switch (strArr[i]) {
+            case "&":
+                strArr[i] = "&amp;";
+                break;
+            case "<":
+                strArr[i] = "&lt;";
+                break;
+            case ">":
+                strArr[i] = "&gt;";
+                break;
+            case "\"":
+                strArr[i] = "&quot;";
+                break;
+            case "'":
+                strArr[i] = "&apos;";
+                break;
+        }
+    }
+
+    console.log(strArr.join(""));
+    // return str;
+}
+
+convertHTML("Dolce & Gabbana");
+
+convertHTML("Dolce & Gabbana");            // should return Dolce &​amp; Gabbana.
+convertHTML("Hamburgers < Pizza < Tacos"); // should return Hamburgers &​lt; Pizza &​lt; Tacos.
+convertHTML("Sixty > twelve");             // should return Sixty &​gt; twelve.
+convertHTML('Stuff in "quotation marks"'); // should return Stuff in &​quot;quotation marks&​quot;.
+convertHTML("Shindler's List");            // should return Shindler&​apos;s List.
+convertHTML("<>");                         // should return &​lt;&​gt;.
+convertHTML("abc");                        // should return abc. 
